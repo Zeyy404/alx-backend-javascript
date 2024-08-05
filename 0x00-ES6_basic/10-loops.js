b489c8c7e9641e6636ae8abd8945ca34bcf35539
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-05 17:50:43 +0400

    Task 10{Modified} + Tasks 11 & 12

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
     const idx = array.indexOf(value);
+    /* eslint-disable no-param-reassign */
     array[idx] = appendString + value;
   }
 
