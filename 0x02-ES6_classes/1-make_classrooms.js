5685d906a7342c046e4d567f09495b5e288d09cf
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-08 14:25:33 +0400

    Tasks 0 - 2

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import Classroom from "./0-classroom.js";
+
+export default function initializeRooms() {
+  const room1 = new Classroom(19);
+  const room2 = new Classroom(20);
+  const room3 = new Classroom(34);
+
+  return [room1, room2, room3];
+}
