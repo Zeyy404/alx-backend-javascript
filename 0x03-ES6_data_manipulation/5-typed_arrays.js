4d98878142ffafbd1e61cb1dc0bd6c633e26194f
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-13 22:51:12 +0400

    Task 5{Modified}

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
+  const dataView = new DataView(buffer);
 
-  if (position < 0 || position >= int8View.length) {
+  if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
-  int8View[position] = value;
-  return int8View;
+  dataView.setInt8(position, value);
+  return dataView;
 }
