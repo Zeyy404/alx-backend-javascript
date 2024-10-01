d68aa9c0c300c361cd8bd01e983a0e99c1ccc258
Zainab Salih <Zainab.Salih@outlook.com>
2024-10-02 00:44:52 +0400

    Task 0 - 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,16 +2,16 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`)
+  console.log(`Your name is: ${input}`);
   rl.close();
 });
 
-rl.on ('close', () => {
-  console.log("This important software is now closing");
+rl.on('close', () => {
+  console.log('This important software is now closing');
 });
